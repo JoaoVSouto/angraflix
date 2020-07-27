14ee1aa0d28f4b27826faf2b0efa8943bf87d6dc
JoaoVSouto <joaovitorss99@hotmail.com>
2020-07-27 15:06:58 -0300

    Creates Carousel component

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,9 +1,59 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import VideoCard from './components/VideoCard';
 
-const Carousel: React.FC = () => {
-  return <div />;
+import {
+  ExtraLink,
+  Title,
+  VideoCardGroupContainer,
+  VideoCardList,
+} from './styles';
+
+interface IProps {
+  ignoreFirstVideo?: boolean;
+  category?: DataCategory;
+}
+
+const Carousel: React.FC<IProps> = ({ category, ignoreFirstVideo }) => {
+  const categoryTitle = category?.titulo;
+  const categoryColor = category?.cor;
+  const categoryExtraLink = category?.link_extra;
+  const videos = category?.videos;
+
+  return (
+    <VideoCardGroupContainer>
+      {category && (
+        <>
+          <Title style={{ backgroundColor: categoryColor || 'red' }}>
+            {categoryTitle}
+          </Title>
+          {categoryExtraLink && (
+            <ExtraLink href={categoryExtraLink.url} target="_blank">
+              {categoryExtraLink.text}
+            </ExtraLink>
+          )}
+        </>
+      )}
+
+      <VideoCardList>
+        {videos?.map((video, index) => {
+          if (ignoreFirstVideo && index === 0) {
+            return null;
+          }
+
+          return (
+            <li key={video.titulo}>
+              <VideoCard
+                videoTitle={video.titulo}
+                videoURL={video.url}
+                categoryColor={categoryColor}
+              />
+            </li>
+          );
+        })}
+      </VideoCardList>
+    </VideoCardGroupContainer>
+  );
 };
 
 export default Carousel;
