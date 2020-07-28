5dc965190d5ac09816f568f2270e82b34fab48e3
JoaoVSouto <joaovitorss99@hotmail.com>
2020-07-28 09:54:37 -0300

    Creates wrapper for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import styled from 'styled-components';
 
 import Menu from './components/Menu';
 import Footer from './components/Footer';
 
 import Routes from './routes';
 
+const Main = styled.main`
+  background-color: #141414;
+  color: var(--white);
+  padding: 50px 5% 0;
+`;
+
 const App: React.FC = () => (
   <BrowserRouter>
     <Menu />
 
-    <Routes />
+    <Main>
+      <Routes />
+    </Main>
 
     <Footer />
   </BrowserRouter>
