e729c951ebbcb2d8c879d260472ee3af27ea67f9
JoaoVSouto <joaovitorss99@hotmail.com>
2020-07-28 19:22:25 -0300

    Changes new video link url and label based on URL

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useMemo } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import { NavMenu, Logo, ButtonLink } from './styles';
 
 import logo from '../../assets/img/logo.png';
 
 const Menu: React.FC = () => {
+  const location = useLocation();
+
+  const isCadastroVideo = useMemo(
+    () => location.pathname === '/cadastro/video',
+    [location]
+  );
+
   return (
     <NavMenu>
       <Logo to="/">
         <img src={logo} alt="AluraFlix" />
       </Logo>
 
-      <ButtonLink as={Link} to="/cadastro/video">
-        Novo vídeo
+      <ButtonLink
+        as={Link}
+        to={isCadastroVideo ? '/cadastro/categoria' : '/cadastro/video'}
+      >
+        {isCadastroVideo ? 'Nova categoria' : 'Novo vídeo'}
       </ButtonLink>
     </NavMenu>
   );
--- src/pages/Cadastro/Video/index.tsx
+++ src/pages/Cadastro/Video/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const CadastroVideo: React.FC = () => {
   return (
     <>
       <h1>Cadastro de Vídeo</h1>
-
-      <Link to="/cadastro/categoria">Cadastrar Categoria</Link>
     </>
   );
 };
