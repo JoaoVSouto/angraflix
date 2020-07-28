997f4898e7448cc9a67d9a1725f7011853065115
JoaoVSouto <joaovitorss99@hotmail.com>
2020-07-28 09:17:13 -0300

    Initializes CadastroVideo component

---
+++ src/pages/Cadastro/Video/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CadastroVideo: React.FC = () => {
+  return <h1 style={{ color: '#fff' }}>Cadastro vídeo!</h1>;
+};
+
+export default CadastroVideo;
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, BrowserRouter, Redirect, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
+import CadastroVideo from './pages/Cadastro/Video';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/cadastro/video" component={CadastroVideo} />
 
       <Redirect to="/" />
     </Switch>
