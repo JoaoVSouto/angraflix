15749c17ffa1895e7b3d00f5d59ff19be5a9b746
JoaoVSouto <joaovitorss99@hotmail.com>
2020-08-02 19:15:19 -0300

    Sets API URL based on env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 const api = axios.create({
-  baseURL: 'http://localhost:3333/',
+  baseURL: isDev
+    ? 'http://localhost:3333/'
+    : 'https://angraflix.herokuapp.com/',
 });
 
 export default api;
